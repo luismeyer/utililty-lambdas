1a1d8d42d3d1b5d18fbfa0df99f4676c1546a0ac
Luis Meyer <luis_meyer@outlook.de>
2021-04-03 22:37:41 +0200

    Add example-lambda

---
+++ example-lambda/src/handler.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandlerV2 = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from the Example Lambda",
+    }),
+  };
+};
